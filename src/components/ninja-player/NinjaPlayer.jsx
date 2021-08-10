96d2ae78d1eb6f9d1a0d9bbba0592bdb7cfc4ec0
montekaka <josh.chen.jia@gmail.com>
2021-08-09 23:30:23 -0700

    adds a storybook for ninja player

--- src/components/ninja-player/NinjaPlayer.jsx
+++ src/components/ninja-player/NinjaPlayer.jsx
@@ -1,39 +1,56 @@
 import React, {useEffect} from 'react';
+import {useAtom} from 'jotai'
 import PropTypes from 'prop-types';
+import {
+  episodesAtom,
+  playerSkinAtom,
+  playingIdAtom
+} from './jotai'
 
 const NinjaPlayer = ({
   podcast, episodes, theme
 }) => {
+
+  // set jotai state
+  const [episodesState, setEpisodes] = useAtom(episodesAtom);
+  const [, setPlayerSkin] = useAtom(playerSkinAtom)
+  const [, setPlayingId] = useAtom(playingIdAtom)
 
   useEffect(() => {
 
   }, [podcast])
 
   useEffect(() => {
-
+    if(episodes && episodes.length > 0) {
+      setEpisodes(episodes);
+      setPlayingId(0);
+    }    
   }, [episodes])
   
   useEffect(() => {
-
+    if(theme && Object.keys(theme).length > 0) {
+      setPlayerSkin(theme); // TODO: use mapping to get the right fileds
+    }
   }, [theme])  
 
   return (
     <div className="jc-player-wrapper">
-      
+      <h2>Hello world</h2>
+      <div>{episodesState.length}</div>
     </div>
   )
 }
 
 NinjaPlayer.propTypes = {
-  podcast: PropTypes.object,
+  // podcast: PropTypes.object,
   episodes: PropTypes.array,
-  theme: propTypes.object
+  // theme: propTypes.object
 }
 
 NinjaPlayer.defaultProps = {
-  podcast: {},
+  // podcast: {},
   episodes: [],
-  theme: {}
+  // theme: {}
 };
 
 
